b2a903516aa9bfaef14af0f5c3c213b20c777145
MrH-Pro-Grammer <harsha.runjaala@gmail.com>
2026-06-10 22:43:59 +0530

    server.js fixed port variable deployment issue still persists

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,73 +1,47 @@
-import express from "express"
+import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
-import cors from "cors";
 
-import notesRoutes from "./routes/notesRoutes.js"
-import { connect } from "mongoose"
-import { connectDB } from "./config/db.js"
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
 
-console.log(process.env.MONGO_URI);
-
-const app = express()
-const PORT = process.env.POR || 5001;
+const app = express();
+const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-
-//middleware
-
-if(process.env.NODE_ENV !== "production"){
-
-    app.use(cors({
-        origin:"http://localhost:5173",
-    }));
+// middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
 }
-    app.use(express.json())
-    app.use(rateLimiter);
-/******* */
-//Simple custom middlewear
-// app.use((req,res,next)=>{
-//     console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
-//     next();
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+app.use(rateLimiter);
+
+// our simple custom middleware
+// app.use((req, res, next) => {
+//   console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
+//   next();
 // });
-/******* */
-// instead of hardcoding /api/notes
-app.use("/api/notes", notesRoutes)
 
-//express middlewear
-//render.com
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+app.use("/api/notes", notesRoutes);
 
-app.get(
-    "*",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-})
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
-// app.get("/api/notes", (req,res)=>{
-//     res.status(200).send("you got 50 Notes");
-// })
-
-// app.post("/aapi/notes", (rs,req)=>{
-//     res.status(201).json({message:"posst created successfully"})
-// })
-
-// app.put("/api/noes", (res,req)=>{
-//     res.status(200).json({message:"Post updated successfully"})
-// })
-
-// app.delete("/api/notes", (res,req)=>{
-//     res.status(200).json({message:"Notes deleted sucessfully"})
-// })
-
-connectDB().then(()=>{
-
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
-});
-
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server started on PORT:", PORT);
+  });
 });
