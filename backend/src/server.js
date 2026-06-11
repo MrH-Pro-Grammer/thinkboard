347bc700e39f6b36333a31774412875ea6f73f60
MrH-Pro-Grammer <harsha.runjaala@gmail.com>
2026-06-11 16:56:42 +0530

    few more syntax fix for ex 4.x , ex 5.0 is unstable ,deployment faild again

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from "url";
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -11,7 +12,8 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
-const __dirname = path.resolve();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // middleware
 if (process.env.NODE_ENV !== "production") {
